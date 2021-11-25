b0659498af013d34648643ba812b836b16977ebd
Mario Junior <drapalskimario@gmail.com>
2021-11-25 00:07:32 -0300

    add User class

---
+++ src/User.java
@@ -0,0 +1,52 @@
+/**
+ * @author mario.junior
+ */
+public class User {
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+
+  public User() {
+  }
+
+  public User(String firstName, String lastName, String email, String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+}
