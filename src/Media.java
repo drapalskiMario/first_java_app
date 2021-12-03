2b8e636044f2f8044d3155e5a84f09be6db306d4
Mario Junior <drapalskimario@gmail.com>
2021-12-03 00:10:24 -0300

    add class Media

---
+++ src/Media.java
@@ -0,0 +1,12 @@
+public class Media {
+    public static void main(String[] args) {
+        double nota1 = 7.8;
+        double nota2 = 9.0;
+        double nota3 = 6.7;
+        double nota4 = 8.0;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média das 4 notas é: " + media);
+    }
+}
