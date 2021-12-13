02e8c4363c4ac005110efab38f991239c3f5e87c
Mario Junior <drapalskimario@gmail.com>
2021-12-13 15:41:29 -0300

    tarefa wrapper

---
+++ src/Wrappers.java
@@ -0,0 +1,11 @@
+public class Wrappers {
+    public static void main(String[] args) {
+        int varPrimitivo = 500;
+
+        System.out.println(varPrimitivo);
+
+        Integer varWrapper = varPrimitivo;
+
+        System.out.println(varWrapper);
+    }
+}
