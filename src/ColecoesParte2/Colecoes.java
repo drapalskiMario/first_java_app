2474d408cc80794b5ff55c97922eadf497affada
Mario Junior <drapalskimario@gmail.com>
2021-12-21 23:10:42 -0300

    Adicionado lição de coleções

--- src/ColecoesParte2/Colecoes.java
+++ src/ColecoesParte2/Colecoes.java
@@ -1,9 +1,7 @@
 package ColecoesParte2;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Colecoes {
 
@@ -11,23 +9,13 @@ public class Colecoes {
         List<String> grupoMasculino = new ArrayList<>();
         List<String> grupoFeminino = new ArrayList<>();
 
-        Set<Pessoa> pessoas = new HashSet<>();
-        Pessoa pessoa1 = new Pessoa("Mario Junior", "Masculino");
-        Pessoa pessoa2 = new Pessoa("Maria Silva", "Feminino");
-        Pessoa pessoa3 = new Pessoa("João Mendes", "Masculino");
-        Pessoa pessoa4 = new Pessoa("Ana Flavia", "Feminino");
+        String nome = "Maria Silva";
+        String sexo = "Feminino";
 
-        pessoas.add(pessoa1);
-        pessoas.add(pessoa2);
-        pessoas.add(pessoa3);
-        pessoas.add(pessoa4);
-
-        for (Pessoa pessoa: pessoas) {
-            if (pessoa.getSexo().equals("Masculino")) {
-                grupoMasculino.add(pessoa.getNome());
-            } else if (pessoa.getSexo().equals("Feminino")) {
-                grupoFeminino.add(pessoa.getNome());
-            }
+        if (sexo.equals("Masculino")) {
+            grupoMasculino.add(nome);
+        } else if (sexo.equals("Feminino")) {
+            grupoFeminino.add(nome);
         }
 
         System.out.println(grupoMasculino);
